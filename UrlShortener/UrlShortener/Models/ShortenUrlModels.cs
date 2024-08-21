df816c867f5d3be80dfce099efd09725b245dd5c
Danijel <glisovic.danijel96@gmail.com>
2024-08-21 12:02:23 +0200

    Added request model. Added FluentValidation.

---
+++ UrlShortener/UrlShortener/Models/ShortenUrlModels.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+
+namespace UrlShortener.Models;
+
+internal sealed class ShortenUrlRequest
+{
+    public ShortenUrlRequest(string url)
+    {
+        Url = url;
+    }
+
+    public string Url { get; init; }
+}
+
+internal sealed class ShortenUrlRequestValidator : AbstractValidator<ShortenUrlRequest>
+{
+    public ShortenUrlRequestValidator()
+    {
+        RuleFor(request => request.Url)
+            .NotEmpty()
+            .Must(LinkMustBeAUri)
+            .WithMessage("Input string is not a valid url value.");
+    }
+
+    private static bool LinkMustBeAUri(string link)
+    {
+        return Uri.TryCreate(link, UriKind.Absolute, out var outUri)
+               && (outUri.Scheme == Uri.UriSchemeHttp || outUri.Scheme == Uri.UriSchemeHttps);
+    }
+}
---
+++ UrlShortener/UrlShortener/WebApplicationEndpointMappings.cs
@@ -0,0 +1,26 @@
+﻿using UrlShortener.Models;
+
+namespace UrlShortener;
+
+internal static class WebApplicationEndpointMappings
+{
+    public static void MapEndpoints(this WebApplication app)
+    {
+        app.MapPost("/shortenurl", (ShortenUrlRequest request) =>
+        {
+            // Should inject the validator
+            var validator = new ShortenUrlRequestValidator();
+            var validationResult = validator.Validate(request);
+
+            if (!validationResult.IsValid)
+            {
+                return Results.ValidationProblem(validationResult.ToDictionary());
+            }
+
+            return Results.Ok();
+            
+        })
+        .WithName("ShortenUrl")
+        .WithOpenApi();
+    }
+}
